69c6fbe0d39a1d9398e51fb55c49249442871367
Marco Pettorali <marcopettorali1@gmail.com>
2021-02-16 16:52:55 +0100

    Implemented login

--- src/main/java/ServletLogin.java
+++ src/main/java/ServletLogin.java
@@ -4,14 +4,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(name = "ServletLogin")
 public class ServletLogin extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    HashMap<String,String> login = new HashMap<>();
+    public ServletLogin(){
+        login.put("pippo", "pippo");
+        login.put("graziano", "graziano");
+        login.put("mariano", "mariano");
+        login.put("silvano", "silvano");
+    }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(login.get(username) == password){
+            response.sendRedirect(request.getContextPath() + "/map.html");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        request.getRequestDispatcher("/html/login.html").forward(request, response);
     }
 }
